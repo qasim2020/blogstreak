fd04b338a49f09d3a962ffd84842f190388f85af
Qasim Ali <qasimali24@gmail.com>
2020-08-31 14:52:28 +0500

    form for profile update added

--- app.js
+++ app.js
@@ -4,13 +4,39 @@ const hbs = require('hbs');
 
 const app = express()
 
+const {
+  resolve
+} = require('path');
+// Copy the .env.example in the root into a .env file in this folder
+require('dotenv').config({
+  path: './.env'
+});
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+app.use(express.static(process.env.STATIC_DIR));
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function(req, res, buf) {
+      if (req.originalUrl.startsWith('/webhook')) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 hbs.registerHelper('match', function(val1,val2) {
   // console.log(val1,val2);
   return val1.toUpperCase() == val2.toUpperCase() ? true : false;
 })
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.status(200).render('index.hbs');
 })
 
 app.get('/blogstreak', (req,res) => {
@@ -49,7 +75,7 @@ app.get('/blogstreak', (req,res) => {
       })
     };
 
-    res.status(200).render('index.hbs', {
+    res.status(200).render('blogstreak.hbs', {
       data: sorted,
       days,
       // token: req.session.token,
@@ -103,6 +129,108 @@ app.get('/blogpost', (req,res,next) => {
       tags: sorted[0].Tags.split(',')
     });
   })
+})
+
+app.get('/linkMedium', (req,res) => {
+  res.status(200).render('linkMedium.hbs');
+})
+
+// Fetch the Checkout Session to display the JSON result on the success page
+app.get('/checkout-session', async (req, res) => {
+  const {
+    sessionId
+  } = req.query;
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  res.send(session);
+});
+
+app.post('/create-checkout-session', async (req, res) => {
+  const domainURL = process.env.DOMAIN;
+
+  const {
+    quantity,
+    locale,
+    userInput
+  } = req.body;
+  console.log(userInput);
+  // Create new Checkout Session for the order
+  // Other optional params include:
+  // [billing_address_collection] - to display billing address details on the page
+  // [customer] - if you have an existing Stripe Customer ID
+  // [customer_email] - lets you prefill the email input in the Checkout page
+  // For full details see https://stripe.com/docs/api/checkout/sessions/create
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: process.env.PAYMENT_METHODS.split(', '),
+    mode: 'subscription',
+    locale: locale,
+    line_items: [{
+      price: process.env.PRICE,
+      quantity: quantity
+    }],
+    // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+    // payment_intent_data: {
+    //   metadata: {
+    //     userInput: userInput
+    //   },
+    // },
+    success_url: `${domainURL}/profile?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${domainURL}/`,
+  });
+
+  res.send({
+    sessionId: session.id,
+  });
+});
+
+// Webhook handler for asynchronous events.
+app.post('/webhook', async (req, res) => {
+  let data;
+  let eventType;
+  // Check if webhook signing is configured.
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers['stripe-signature'];
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  if (eventType === 'checkout.session.completed') {
+    console.log(`🔔  Payment received!`);
+  }
+
+  res.sendStatus(200);
+});
+
+app.get('/config', async (req, res) => {
+  const price = await stripe.prices.retrieve(process.env.PRICE);
+
+  res.send({
+    publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    unitAmount: price.unit_amount,
+    currency: price.currency,
+  });
+});
+
+app.get('/profile', (req,res) => {
+  return res.status(200).render('profile.hbs')
 })
 
 
